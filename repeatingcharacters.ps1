fe8ea887b194418edd27d993c52c5bedf7b43e19
vidshadow <38868020+vidshadow@users.noreply.github.com>
2020-11-04 13:28:40 -0700

    Add files via upload

---
+++ repeatingcharacters.ps1
@@ -0,0 +1,14 @@
+﻿function get-duplicatechar {
+ [CmdletBinding()]
+  param (
+     [string]$teststring
+   )
+   $DupeArray = ($teststring.ToCharArray() | Group | Select Count, Name | Where -Property Count -ne 1)
+if($teststring.ToCharArray() | Group | Select Count, Name | Where -Property Count -ne 1)
+{
+$DupeArray[0] | Select Name
+}
+else
+{
+"All Unique"
+}}
